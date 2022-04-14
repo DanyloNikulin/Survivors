caca7d97bac15b7d631dd37bd256d350c8a8ffdf
Olexander Belenko <d410227@gmail.com>
2022-04-14 21:23:58 +0300

    Fixed project context prefab Refactored codestyle

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
-using Zenject; 
-
-
+using Zenject;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CharacterMovement: MonoBehaviour
 {
-	Rigidbody2D _body;
-	[SerializeField] float _speed;
+	[SerializeField] private float _speed;
+	
+	private Rigidbody2D _body;
+	
 	[Inject]
 	public void Init(Rigidbody2D body) {
 		_body = body;
 	}
+	
 	public void Move(Vector2 direction) {
 		_body.velocity = direction * _speed;
 	}
