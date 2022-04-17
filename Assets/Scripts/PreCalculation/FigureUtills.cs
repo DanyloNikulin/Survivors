93054705a6e746a7076e80b93ca372743f836b70
DanyloNikulin <83497040+DanyloNikulin@users.noreply.github.com>
2022-04-17 14:21:03 +0300

    test squre spawn

--- Assets/Scripts/PreCalculation/FigureUtills.cs
+++ Assets/Scripts/PreCalculation/FigureUtills.cs
@@ -1,10 +1,10 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class FigureUtills {
 
     private static List<Vector2> Circle;
+    private static List<Vector2> Square;
 
     public static List<Vector2>  CalculateCircle(int scale) { 
         Circle = new List<Vector2>();
@@ -13,4 +13,21 @@ public static class FigureUtills {
         }
         return Circle;
     }
+
+    public static List<Vector2> CalculateSquare(int scale) {
+        Square = new List<Vector2>();
+        Square.Add (new Vector2(1,1));
+        Square.Add(new Vector2(-1, -1));
+        Square.Add(new Vector2(1, -1));
+        Square.Add(new Vector2(-1, 1));
+
+        for (float theta = 0; theta < (scale-4)/ 4; theta += 1/ ((scale-4)/4)) {
+            Square.Add(new Vector2(1 - theta, 1));
+            Square.Add(new Vector2(-1 , -1 + theta));
+            Square.Add(new Vector2(1, -1 + theta));
+            Square.Add(new Vector2(-1 + theta, 1));
+        }
+        return Square;
+    }
+    
 }
